52abcb86e08a2867e535e583eef39d23c46478af
Jack Schiesl <jack.schiesl@gmail.com>
2020-09-29 16:55:21 -0400

    pytest ini; pylint ok; smoke ok

---
+++ converters_test.py
@@ -0,0 +1,10 @@
+"""Unit tests for the unit converter library functions"""
+
+def describe_a_library_of_units_converters():
+    
+    """Test suite for units conversion functions"""
+    # pylint: disable=unused-variable
+
+    def blows_smoke():
+      assert True
+    
